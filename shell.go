fda62c8c1a043a52bd0ba3107e131c080f4fba82
bgpat <bgpat@users.noreply.github.com>
2019-04-24 22:05:32 +0900

    WIP

--- shell.go
+++ shell.go
@@ -1,5 +1,16 @@
 package gomplete
 
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+var (
+	shellsMu sync.RWMutex
+	shells   = make(map[string]Shell)
+)
+
 // A Shell is the shell completion interface.
 type Shell interface {
 	// FormatReply converts the completion reply to the string the script output by Script() can parse.
@@ -7,4 +18,36 @@ type Shell interface {
 
 	// Script outputs the shell script to parse the reply and register the completion.
 	Script(cmdline string) string
+}
+
+// RegisterShell makes a shell implementation available by the provided name.
+// If RegisterShell is called twice with the same name or if shell is nil, it returns an error.
+func RegisterShell(name string, shell Shell) error {
+	shellsMu.Lock()
+	defer shellsMu.Unlock()
+	if shell == nil {
+		return errors.New("shell is nil")
+	}
+	if _, dup := shells[name]; dup {
+		return fmt.Errorf("%q is already registered", name)
+	}
+	shells[name] = shell
+	return nil
+}
+
+// Shells returns a list of the names of the registered shells.
+func Shells() []string {
+	shellsMu.RLock()
+	defer shellsMu.RUnlock()
+	list := make([]string, 0, len(shells))
+	for name := range shells {
+		list = append(list, name)
+	}
+	return list
+}
+
+func FormatReply(name string, reply Reply) (string, error) {
+	shellsMu.RLock()
+	shellsMu.RUnlock()
+	return "", nil
 }
