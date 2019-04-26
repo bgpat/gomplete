7c2d9ebd639c93f6789e920c621ceaff12fb9ddc
bgpat <bgpat@users.noreply.github.com>
2019-04-26 09:10:25 +0900

    Implement e2e test

--- shells/bash/bash_test.go
+++ shells/bash/bash_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/bradleyjkemp/cupaloy"
-	_ "github.com/kr/pty"
 
 	"github.com/bgpat/gomplete"
 )
@@ -94,71 +93,3 @@ func TestOutputScript(t *testing.T) {
 		})
 	}
 }
-
-/*
-func TestScript(t *testing.T) {
-	dir, err := ioutil.TempDir("", "example")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-
-	binfile := filepath.Join(dir, "example")
-	excmd := exec.Command("go", "build", "-o", binfile, "../../examples/bash")
-	if err := excmd.Run(); err != nil {
-		t.Error(err)
-	}
-
-	compfile := filepath.Join(dir, "example.completion")
-	shell := Shell{Name: "example"}
-	script := shell.Script(binfile + " -completion --")
-	if err := ioutil.WriteFile(compfile, []byte(script), 0644); err != nil {
-		t.Error(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-	defer cancel()
-	cmd := exec.CommandContext(ctx, "bash", "--noprofile", "--norc", "-o", "errexit")
-	cmd.Env = []string{"PATH=" + dir}
-	tty, err := pty.Start(cmd)
-	fmt.Fprintf(tty, "source %q\n", compfile)
-	if err := writeAndWait(ctx, tty, "example "); err != nil {
-		t.Error(err)
-	}
-	tty.WriteString("\t")
-	if err := waitString(ctx, tty, "foo"); err != nil {
-		t.Error(err)
-	}
-	tty.WriteString("\t")
-	if err := waitString(ctx, tty, "bar"); err != nil {
-		t.Error(err)
-	}
-}
-
-func writeAndWait(ctx context.Context, tty io.ReadWriter, s string) error {
-	io.WriteString(tty, s)
-	return waitString(ctx, tty, s)
-}
-
-func waitString(ctx context.Context, tty io.Reader, s string) error {
-	ticker := time.NewTicker(time.Millisecond)
-	defer ticker.Stop()
-	text := ""
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case <-ticker.C:
-			buf := make([]byte, 1024)
-			n, err := tty.Read(buf)
-			if err != nil {
-				return err
-			}
-			text += string(buf[:n])
-			if strings.LastIndex(text, s) >= 0 {
-				return nil
-			}
-		}
-	}
-}
-*/
