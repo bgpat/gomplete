42999a6bbc01ef24484e49cc68cc6b928b62b635
bgpat <bgpat@users.noreply.github.com>
2019-04-28 21:47:43 +0900

    Add e2e test for fish

---
+++ _test/e2e/fish_test.go
@@ -0,0 +1,79 @@
+// +build !bash,!zsh
+
+package e2e
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/kr/pty"
+)
+
+func TestFish(t *testing.T) {
+	dir, err := ioutil.TempDir("", "example")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	binfile := filepath.Join(dir, "examples")
+	excmd := exec.Command("go", "build", "-o", binfile, "../../examples")
+	if err := excmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "fish")
+	cmd.Env = []string{
+		"PATH=" + dir,
+		"TERM=vt100",
+	}
+	cmd.Env = append(cmd.Env, os.Environ()...)
+	tty, err := pty.Start(cmd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := tty.WriteString("source (examples -completion=fish | psub)\n"); err != nil {
+		t.Fatal(err)
+	}
+	if err := writeAndWait(ctx, tty, "examples "); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := tty.WriteString("\t"); err != nil {
+		t.Fatal(err)
+	}
+	reply, err := waitString(ctx, tty, "foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("reply: %q", reply)
+	if _, err := tty.WriteString("\t"); err != nil {
+		t.Fatal(err)
+	}
+	reply, err = waitString(ctx, tty, "bar")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("reply: %q", reply)
+	if _, err := tty.WriteString("\t\t"); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("reply: %q", reply)
+	reply, err = waitString(ctx, tty, "\r\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("reply: %q", reply)
+	for _, arg := range []string{"hoge", "fuga", "piyo"} {
+		if !strings.Contains(reply, arg) {
+			t.Errorf("3rd sub-commands must include %q", arg)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bgpat/gomplete
 require (
 	github.com/bradleyjkemp/cupaloy v2.3.0+incompatible
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pty v1.1.4 // indirect
+	github.com/kr/pty v1.1.4
 	github.com/mattn/go-isatty v0.0.7
 	github.com/pkg/errors v0.8.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
