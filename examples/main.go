fbbcfef040d0c28e6568c52959b3d928f1ae8a5e
bgpat <bgpat@users.noreply.github.com>
2019-04-29 12:58:07 +0900

    Relocate plugins

--- examples/main.go
+++ examples/main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 
 	"github.com/bgpat/gomplete"
-	"github.com/bgpat/gomplete/plugins"
+	gomplete_flag "github.com/bgpat/gomplete/plugins/flag"
 )
 
 func main() {
@@ -31,7 +31,7 @@ func main() {
 		},
 	}
 	flag.Var(
-		&plugins.Flag{
+		&gomplete_flag.Value{
 			Completion: &comp,
 			FlagName:   "completion",
 		},
--- plugins/flag/value.go
+++ plugins/flag/value.go
@@ -1,4 +1,4 @@
-package plugins
+package flag
 
 import (
 	"context"
@@ -15,20 +15,20 @@ import (
 	_ "github.com/bgpat/gomplete/shells/zsh"  // support zsh
 )
 
-// Flag is a implementation for flag.Value.
-type Flag struct {
+// Value is a implementation for flag.Value.
+type Value struct {
 	Completion gomplete.Completion
 	FlagName   string
 }
 
 // String returns empty string.
 // This is a dummy function.
-func (f *Flag) String() string {
+func (v *Value) String() string {
 	return ""
 }
 
 // Set aborts parsing flags and runs the shell completion.
-func (f *Flag) Set(name string) error {
+func (v *Value) Set(name string) error {
 	if name == "true" {
 		fmt.Printf("specify shell: %v\n", gomplete.Shells())
 		fmt.Println("example: -completion=bash")
@@ -54,11 +54,11 @@ func (f *Flag) Set(name string) error {
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	reply := f.Completion.Complete(context.Background(), shell.Args())
+	reply := v.Completion.Complete(context.Background(), shell.Args())
 	return errors.WithStack(shell.FormatReply(reply, os.Stdout))
 }
 
 // IsBoolFlag is a method to meet flag.boolValue.
-func (f *Flag) IsBoolFlag() bool {
+func (v *Value) IsBoolFlag() bool {
 	return true
 }
